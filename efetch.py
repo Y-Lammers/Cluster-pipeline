6c6299e9344010397094978412d5e7c649e9ba1f
Youri Lammers <youri_lammers_88@hotmail.com>
2012-02-15 10:00:34 +0100

    removed wrong uploads

--- efetch.py
+++
@@ -1,8 +0,0 @@
-from Bio import Entrez, SeqIO
-
-code = 301040922
-
-handle = Entrez.efetch(db="nucleotide", id= code, rettype="gb")
-record = SeqIO.read(handle, "genbank")
-handle.close()
-print(record)
